e9f4e294894a40288ab83d73e741d782e25d926e
joabbremer <joab.bremer@gmail.com>
2016-10-23 00:42:16 -0200

    CorsRepair

--- src/com/swapit/ws/service/CORSFilter.java
+++ src/com/swapit/ws/service/CORSFilter.java
@@ -1,22 +1,33 @@
 package com.swapit.ws.service;
 
 import java.io.IOException;
+import java.util.logging.Logger;
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerResponseContext;
 import javax.ws.rs.container.ContainerResponseFilter;
+import javax.ws.rs.container.PreMatching;
 import javax.ws.rs.ext.Provider;
-
+ 
 @Provider
+@PreMatching
 public class CORSFilter implements ContainerResponseFilter {
-
+ 
+    private final static Logger log = Logger.getLogger( CORSFilter.class.getName() );
+ 
     @Override
-    public void filter(ContainerRequestContext request,
-            ContainerResponseContext response) throws IOException {
-        response.getHeaders().add("Access-Control-Allow-Origin", "*");
-        response.getHeaders().add("Access-Control-Allow-Headers",
+    public void filter( ContainerRequestContext requestCtx, ContainerResponseContext responseCtx ) throws IOException {
+        log.info( "Executing REST response filter" );
+ 
+        //responseCtx.getHeaders().add( "Access-Control-Allow-Origin", "*" );
+        //responseCtx.getHeaders().add( "Access-Control-Allow-Credentials", "true" );
+        //responseCtx.getHeaders().add( "Access-Control-Allow-Methods", "GET, POST, DELETE, PUT" );
+        
+        responseCtx.getHeaders().add("Access-Control-Allow-Origin", "*");
+        responseCtx.getHeaders().add("Access-Control-Allow-Headers",
                 "origin, content-type, accept, authorization");
-        response.getHeaders().add("Access-Control-Allow-Credentials", "true");
-        response.getHeaders().add("Access-Control-Allow-Methods",
+        responseCtx.getHeaders().add("Access-Control-Allow-Credentials", "true");
+        responseCtx.getHeaders().add("Access-Control-Allow-Methods",
                 "GET, POST, PUT, DELETE, OPTIONS, HEAD");
     }
+    
 }
